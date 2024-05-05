7d37b74874d57d9a078b0fd164c4f37ab2228b31
Chris Gilliam <Chris@comcorp.co.za>
2024-05-05 12:44:02 +0200

    Added file upload

--- LinkSharing App/Pages/ProfileDetails.razor.cs
+++ LinkSharing App/Pages/ProfileDetails.razor.cs
@@ -1,6 +1,26 @@
-﻿namespace LinkSharing_App.Pages
+﻿using LinkSharing_App.ViewModels;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace LinkSharing_App.Pages
 {
     partial class ProfileDetails
     {
+        public ProfileDetailsViewModel ProfilePhotoViewModel { get; set; } = new ();
+        private String _photoString;
+        private String _photoFormat;
+        private byte[] _photoBytes;
+        public async void OnPhotoUploaded(InputFileChangeEventArgs e)
+        {
+            if (e.File!=null)
+            {
+                using var memoryStream = new MemoryStream();
+                await (e.File.OpenReadStream().CopyToAsync(memoryStream));
+                _photoBytes = memoryStream.ToArray();
+                _photoString = Convert.ToBase64String(_photoBytes);
+                _photoFormat = e.File.ContentType;
+            }
+         
+        }
+
     }
 }
---
+++ LinkSharing App/ViewModels/ProfileDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkSharing_App.ViewModels
+{
+    public class ProfileDetailsViewModel
+    {
+        public String Name { get; set; }
+        public String LastName { get; set; }
+        public String EmailAddress { get; set; }
+
+        public String Photo {  get; set; }
+        public String PhotoFormat { get; set; }
+
+
+    }
+}
